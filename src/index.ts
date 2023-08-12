5fd9dfe7f6e9360a4c851e3c9143c8933e3c9e1b
zhangjing-GitHub-Code <zhangjing20091105@126.com>
2023-08-12 16:53:22 +0800

    output receive ban name

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,11 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
+  //ctx.on('command');
+  ctx.command("ban <user:user>")
+    .action((_,user)=>{
+      return "receive ban user "+user;
+      //_.session.bot.setGroupBan();
+    })
   // write your plugin here
 }
